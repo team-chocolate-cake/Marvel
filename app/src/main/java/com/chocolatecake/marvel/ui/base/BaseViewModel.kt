c2f5ffbbff989a2c789a796ac5ff8cab156e5867
Ahmed Ghannam <iia7mt@gmail.com>
2023-04-29 14:17:47 +0300

    add composite disposable object

--- app/src/main/java/com/chocolatecake/marvel/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/base/BaseViewModel.kt
@@ -3,10 +3,11 @@ package com.chocolatecake.marvel.ui.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 abstract class BaseViewModel : ViewModel() {
     protected val repository = null // its type should be Repository,but it is not created
-
+    protected val compositeDisposable = CompositeDisposable()
     private val _error = MutableLiveData<Throwable>()
     val error: LiveData<Throwable>
         get() = _error
@@ -18,4 +19,9 @@ abstract class BaseViewModel : ViewModel() {
     private val _isSuccess = MutableLiveData<Boolean>()
     val isSuccess: LiveData<Boolean>
         get() = _isSuccess
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 }
