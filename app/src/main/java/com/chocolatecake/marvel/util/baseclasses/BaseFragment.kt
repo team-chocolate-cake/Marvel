cfa41a0d929cc14e1689028696ffc9cdc46f48ae
Ahmed Ghannam <iia7mt@gmail.com>
2023-04-29 08:26:43 +0300

    create base fragment class

---
+++ app/src/main/java/com/chocolatecake/marvel/util/baseclasses/BaseFragment.kt
@@ -0,0 +1,33 @@
+package com.chocolatecake.marvel.util.baseclasses
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = _binding as VB
+
+    abstract val inflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflater(inflater, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUp()
+    }
+
+    abstract fun setUp()
+
+}
