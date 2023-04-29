5767b32de3c9afcda91b433605cf242face6a99a
jinan <jinanaqb@gmail.com>
2023-04-29 14:23:14 +0300

    add status to binding adapter

--- app/src/main/java/com/chocolatecake/marvel/ui/util/BindingAdapters.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/util/BindingAdapters.kt
@@ -1,2 +1,21 @@
 package com.chocolatecake.marvel.ui.util
 
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import com.chocolatecake.marvel.data.util.State
+
+@BindingAdapter(value = ["app:showWhenSuccess"])
+fun <T> View.showWhenSuccess(state: State<T>?) {
+    this.isVisible = (state is State.Success)
+}
+
+@BindingAdapter(value = ["app:showWhenFailure"])
+fun <T> View.showWhenFailure(state: State<T>?) {
+    this.isVisible = (state is State.Failure)
+}
+
+@BindingAdapter(value = ["app:showWhenLoading"])
+fun <T> View.showWhenLoading(state: State<T>?) {
+    this.isVisible = (state is State.Loading)
+}
