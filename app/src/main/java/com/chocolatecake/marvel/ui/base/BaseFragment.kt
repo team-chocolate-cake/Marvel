8c5e6a2597e26af540f842ec86a954318f281131
Ahmed Ghannam <iia7mt@gmail.com>
2023-04-29 16:05:27 +0300

    change view binding to data binding

--- app/src/main/java/com/chocolatecake/marvel/ui/base/BaseFragment.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/base/BaseFragment.kt
@@ -4,23 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.viewbinding.ViewBinding
+import androidx.lifecycle.ViewModelProvider
 
-abstract class BaseFragment<VB : ViewBinding> : Fragment() {
-    private var _binding: VB? = null
-    protected val binding: VB
-        get() = _binding as VB
+abstract class BaseFragment<VDB : ViewDataBinding, VM : BaseViewModel> : Fragment() {
+    private var _binding: VDB? = null
+    protected val binding: VDB
+        get() = _binding as VDB
 
-    abstract val inflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+    protected lateinit var viewModel:VM
+    abstract val viewModelClass: Class<VM>
+    abstract val inflater: (LayoutInflater, ViewGroup?, Boolean) -> VDB
+    abstract val layoutIdFragment: Int
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = inflater(inflater, container, false)
-        return super.onCreateView(inflater, container, savedInstanceState)
+        viewModel = ViewModelProvider(this)[viewModelClass]
+        _binding = DataBindingUtil.inflate<VDB>(inflater, layoutIdFragment, container, false)
+        _binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
