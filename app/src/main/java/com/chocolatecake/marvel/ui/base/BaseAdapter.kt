4ea14067e4e8a9bea8cbacd30560779cbc449b6b
Banan Kamal <bananhmdan962@gmail.com>
2023-04-30 20:19:37 +0300

    edit onBindViewHolder function and make setRecyclerItems as extension function

--- app/src/main/java/com/chocolatecake/marvel/ui/base/BaseAdapter.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/base/BaseAdapter.kt
@@ -5,6 +5,7 @@
     import android.view.ViewGroup
     import androidx.databinding.DataBindingUtil
     import androidx.databinding.ViewDataBinding
+    import androidx.recyclerview.widget.DiffUtil
     import androidx.recyclerview.widget.RecyclerView
     import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
@@ -15,22 +16,48 @@
 
           abstract val layoutId:Int
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-            return ItemViewHolder(DataBindingUtil.inflate(LayoutInflater.from(parent.context),layoutId,parent,false))
+            return ItemViewHolder(
+                DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    layoutId,
+                    parent,
+                    false)
+            )
+        }
+        override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+            val currentItem = items[position]
+            when (holder) {
+                is ItemViewHolder -> {
+                    holder.binding.setVariable(BR.viewModel, currentItem)
+                    holder.binding.setVariable(BR.listener, listener)
+                }
+            }
         }
 
-       abstract override fun onBindViewHolder(holder: BaseViewHolder, position: Int)
 
-
-        fun setItems(newItem:List<T>){
-            items=newItem
+        fun setItems(newItems: List<T>) {
+            val diffUtil = DiffUtil.calculateDiff(DiffUtils(items, newItems))
+            items = newItems
+            diffUtil.dispatchUpdatesTo(this)
         }
-
         fun getItems()= items
-
         override fun getItemCount()=items.size
 
-        class ItemViewHolder(binding:ViewDataBinding):BaseViewHolder(binding)
+        interface BaseInteractionListener
+
+        class ItemViewHolder(binding: ViewDataBinding):BaseViewHolder(binding)
+
         abstract class BaseViewHolder(itemView: ViewDataBinding) :ViewHolder(itemView.root)
 
+        inner class DiffUtils<T>(private val oldList: List<T>, private val newList: List<T>) : DiffUtil.Callback() {
+            override fun getOldListSize() = oldList.size
+            override fun getNewListSize() = newList.size
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+                oldList[oldItemPosition] == newList[newItemPosition]
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
+
         }
-    interface BaseInteractionListener
+
+        }
+
+
