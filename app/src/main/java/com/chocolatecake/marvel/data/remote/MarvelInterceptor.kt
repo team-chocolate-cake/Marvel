926f616f57cea50a8e4225f1002e7b91469700c8
Najeia Tammous <ntammous11@gmail.com>
2023-04-30 22:04:42 +0300

    Implement interceptor and Generate Hash Classes

--- app/src/main/java/com/chocolatecake/marvel/data/remote/MarvelInterceptor.kt
+++ app/src/main/java/com/chocolatecake/marvel/data/remote/MarvelInterceptor.kt
@@ -1,4 +1,26 @@
 package com.chocolatecake.marvel.data.remote
 
-class MarvelInterceptor {
+import com.chocolatecake.marvel.BuildConfig
+import com.chocolatecake.marvel.data.util.GenerateHash
+import okhttp3.Interceptor
+import okhttp3.Response
+
+
+class MarvelInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val timestamp = System.currentTimeMillis().toString()
+        val hash = GenerateHash().generate(timestamp)
+
+        val originalRequest = chain.request()
+        val url = originalRequest.url.newBuilder()
+            .addQueryParameter("ts", timestamp)
+            .addQueryParameter("apikey", BuildConfig.PUBLIC_KEY)
+            .addQueryParameter("hash", hash)
+            .build()
+
+        val requestBuilder = originalRequest.newBuilder()
+            .url(url)
+
+        return chain.proceed(requestBuilder.build())
+    }
 }
--- app/src/main/java/com/chocolatecake/marvel/data/util/GenerateHash.kt
+++ app/src/main/java/com/chocolatecake/marvel/data/util/GenerateHash.kt
@@ -1,4 +1,14 @@
 package com.chocolatecake.marvel.data.util
 
-class GenerateHash {
+import com.chocolatecake.marvel.BuildConfig
+import java.security.MessageDigest
+
+class GenerateHash() {
+    fun generate(timestamp: String): String {
+        val md5 = MessageDigest.getInstance("MD5")
+        val input = "$timestamp${BuildConfig.PRIVATE_KEY}${BuildConfig.PUBLIC_KEY}"
+        val digest = md5.digest(input.toByteArray())
+
+        return digest.joinToString("") { "%02x".format(it) }
+    }
 }
