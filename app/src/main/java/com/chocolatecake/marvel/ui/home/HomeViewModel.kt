5e369bf9322499261f1126db2639bd0c829442fb
mohammed-akkad <momoo362514@gmail.com>
2023-05-01 20:14:29 +0300

    add home view model

---
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.chocolatecake.marvel.ui.home
+
+import androidx.lifecycle.ViewModel
+import com.chocolatecake.marvel.ui.base.BaseViewModel
+
+class HomeViewModel : BaseViewModel() {
+
+}
