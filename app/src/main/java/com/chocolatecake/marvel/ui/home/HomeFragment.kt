c618aaf74b813726c574467327a0a8e4f664d9c5
mohammed-akkad <momoo362514@gmail.com>
2023-05-01 20:13:44 +0300

    add home fragment

---
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/HomeFragment.kt
@@ -0,0 +1,17 @@
+package com.chocolatecake.marvel.ui.home
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.chocolatecake.marvel.databinding.FragmentHomeBinding
+import com.chocolatecake.marvel.ui.base.BaseFragment
+
+class HomeFragment() : BaseFragment<FragmentHomeBinding,HomeViewModel>()  {
+    override val viewModelClass: Class<HomeViewModel>
+        get() = TODO("Not yet implemented")
+    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
+        get() = TODO("Not yet implemented")
+    override val layoutIdFragment: Int
+        get() = TODO("Not yet implemented")
+
+}
