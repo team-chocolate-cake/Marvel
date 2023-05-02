74da298b78b36510135f59efcdd67a28fdf0a89d
Banan Kamal <bananhmdan962@gmail.com>
2023-05-02 07:11:10 +0300

    make XML Search fragment

---
+++ app/src/main/java/com/chocolatecake/marvel/ui/search/SearchFragment.kt
@@ -0,0 +1,16 @@
+package com.chocolatecake.marvel.ui.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+
+class SearchFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
