b9f9a0920bdbb427d506744f119ec33e71937000
nahed <nahedshehata>
2023-05-04 00:09:06 +0300

    add design

---
+++ app/src/main/java/com/chocolatecake/marvel/CreatorDetails.kt
@@ -0,0 +1,20 @@
+package com.chocolatecake.marvel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CreatorDetails : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_creator_details, container, false)
+    }
+
+}
