6f29e16e5a875d70fbee672c03a525ac157bd67a
mohammed-akkad <momoo362514@gmail.com>
2023-05-03 19:29:27 +0300

    fix bugs

--- app/src/main/java/com/chocolatecake/marvel/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/HomeFragment.kt
@@ -3,15 +3,15 @@ package com.chocolatecake.marvel.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.chocolatecake.marvel.R
 import com.chocolatecake.marvel.databinding.FragmentHomeBinding
 import com.chocolatecake.marvel.ui.base.BaseFragment
 
-class HomeFragment() : BaseFragment<FragmentHomeBinding,HomeViewModel>()  {
-    override val viewModelClass: Class<HomeViewModel>
-        get() = TODO("Not yet implemented")
-    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
-        get() = TODO("Not yet implemented")
+class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>()  {
+    override val viewModel: HomeViewModel by viewModels()
+
     override val layoutIdFragment: Int
-        get() = TODO("Not yet implemented")
+        get() =  R.layout.fragment_home
 
 }
