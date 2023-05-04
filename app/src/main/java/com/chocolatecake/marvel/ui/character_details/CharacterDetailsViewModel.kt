e3f2da80d7e0a4d0a3ce404538e51c00e09525a0
Saif Alhaider <saifharith90@gmail.com>
2023-05-04 19:27:06 +0300

    display:character details

--- app/src/main/java/com/chocolatecake/marvel/ui/character_details/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/character_details/CharacterDetailsViewModel.kt
@@ -1,9 +1,56 @@
 package com.chocolatecake.marvel.ui.character_details
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.chocolatecake.marvel.data.model.ComicsResult
+import com.chocolatecake.marvel.data.model.ProfileResult
+import com.chocolatecake.marvel.data.model.base.BaseResponse
 import com.chocolatecake.marvel.data.repository.MarvelRepository
 import com.chocolatecake.marvel.data.repository.MarvelRepositoryImpl
+import com.chocolatecake.marvel.data.util.Status
 import com.chocolatecake.marvel.ui.base.BaseViewModel
 
 class CharacterDetailsViewModel : BaseViewModel() {
     private val repository: MarvelRepository by lazy { MarvelRepositoryImpl() }
+    private val _comics = MutableLiveData<Status<List<ComicsResult>>>()
+    private val _character = MutableLiveData<Status<ProfileResult>>()
+    val comics: LiveData<Status<List<ComicsResult>>> get() = _comics
+    val character: LiveData<Status<ProfileResult>> get() = _character
+
+    init {
+        loadCharacter()
+    }
+
+    fun loadCharacter() {
+        _character.postValue(Status.Loading)
+        repository.getCharacterById(1017100)
+            .subscribe(::onCharacterSuccess, ::onCharacterFailure)
+            .add()
+    }
+
+    fun loadComics() {
+        _comics.postValue(Status.Loading)
+        repository.getComicsForCharacter(1017100)
+            .subscribe(::onComicsSuccess, ::onCharacterFailure)
+            .add()
+    }
+
+    private fun onComicsSuccess(status: Status<BaseResponse<ComicsResult>?>) {
+        status.toData()?.data?.results?.let {
+            _comics.postValue(Status.Success(it.filterNotNull()))
+        }
+    }
+
+    private fun onCharacterSuccess(status: Status<BaseResponse<ProfileResult>?>) {
+        status.toData()?.data?.results?.first().let {
+            _character.postValue(Status.Success(it!!))
+        }
+    }
+
+    private fun onCharacterFailure(throwable: Throwable) {
+        _character.postValue(Status.Failure(throwable.message.toString()))
+        _comics.postValue(Status.Failure(throwable.message.toString()))
+    }
+
+
 }
--- app/src/main/java/com/chocolatecake/marvel/ui/util/BindingAdapters.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/util/BindingAdapters.kt
@@ -4,7 +4,10 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.chocolatecake.marvel.ui.base.BaseAdapter
 import android.view.View
+import android.widget.ImageView
 import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.chocolatecake.marvel.data.model.ImageResponse
 import com.chocolatecake.marvel.data.util.Status
 
 @BindingAdapter(value =["app:items"])
@@ -25,4 +28,17 @@ fun <T> View.showWhenFailure(status: Status<T>?) {
 @BindingAdapter(value = ["app:showWhenLoading"])
 fun <T> View.showWhenLoading(status: Status<T>?) {
     this.isVisible = (status is Status.Loading)
+}
+@BindingAdapter(value = ["app:imageUrl"])
+fun ImageView.loadImage(imageResponse: ImageResponse?) {
+    val url = if (imageResponse?.path?.split("/")?.last() == "image_not_available") {
+        "https://fandomwire.com/wp-content/uploads/2018/07/Marvel-Logo-14.jpg"
+    } else {
+        imageResponse?.toUrl()
+    }
+    Glide.with(context)
+        .load(url)
+        .fitCenter()
+        .centerCrop()
+        .into(this)
 }
