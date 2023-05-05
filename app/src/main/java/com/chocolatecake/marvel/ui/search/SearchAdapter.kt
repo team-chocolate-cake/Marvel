a25f2a0957f9e70cd073a59258088c32340236c8
Banan Kamal <bananhmdan962@gmail.com>
2023-05-06 02:23:11 +0300

    refactor search fragment code

--- app/src/main/java/com/chocolatecake/marvel/ui/search/SearchAdapter.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/search/SearchAdapter.kt
@@ -1,74 +1,122 @@
 package com.chocolatecake.marvel.ui.search
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import com.chocolatecake.marvel.R
 import com.chocolatecake.marvel.ui.base.BaseAdapter
 
 
-class SearchAdapter( items: List<Any>, listener: SearchInteractionListener) :
-    BaseAdapter<Any>(items, listener) {
-    var myItems=items
-    fun updateList(newItems: List<Any>) {
-        val diffResult = DiffUtil.calculateDiff(DiffUtils(myItems, newItems))
-        myItems = newItems
-        diffResult.dispatchUpdatesTo(this)
-    }
-    override val layoutId: Int
-        get() = R.layout.item_series
-}
-
-
-
-
-//import android.view.LayoutInflater
-//import android.view.ViewGroup
-//import androidx.databinding.DataBindingUtil
-//import com.chocolatecake.marvel.R
+//class SearchAdapter(var items: List<Any>, listener: SearchInteractionListener) :
+//    BaseAdapter<Any>(items, listener) {
 //
-//import com.chocolatecake.marvel.databinding.ItemSeriesBinding
-//import com.chocolatecake.marvel.ui.base.BaseAdapter
+//    fun updateList(newItems: List<Any>) {
+//        val oldItems = items
+//        items = newItems
+//        val diffResult = DiffUtil.calculateDiff(DiffUtils(oldItems, newItems))
+//        diffResult.dispatchUpdatesTo(this)
+//    }
+
+//    override fun getItemCount() = items.size
 //
-//class SearchAdapter(
-//   val itemsList:List<SearchItems>,
-//    listener:SearchInteractionListener
-//    ):BaseAdapter<SearchItems>(itemsList,listener) {
+//    override fun onBindViewHolder(holder: BaseViewHolder<Any>, position: Int) {
+//        holder.bind(items[position])
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<Any> {
+//        val itemView = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
+//        return ItemViewHolder(itemView, listener)
+//    }
+//
 //    override val layoutId: Int
 //        get() = R.layout.item_series
 //
-//
-//    class SeriesViewHolder(var itemSeriesBinding:ItemSeriesBinding ):BaseViewHolder(itemSeriesBinding)
-//    private fun onBindSeries(searchHeaderViewHolder: SeriesViewHolder, position:Int){
-//        val header = itemsList[position] as SearchItems.SeriesItem
-//        searchHeaderViewHolder.itemSeriesBinding.item=header.series
-//    }
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-//        return when(viewType){
-//             VIEW_SERIES_HEADER->{
-//                 val binding=DataBindingUtil.inflate<ItemSeriesBinding>(
-//                     LayoutInflater.from(parent.context),
-//                     R.layout.item_series,
-//                     parent,
-//                     false)
-//                 SeriesViewHolder(binding)
-//             }
-//            else -> throw Exception("Invalid ViewType")
+//    private class ItemViewHolder(itemView: View, listener: SearchInteractionListener) :
+//        BaseViewHolder<Any>(itemView, listener) {
+//        override fun bind(item: Any) {
+//            // Bind data to views in the ViewHolder
 //        }
 //    }
-//
-//    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-//        when(holder){
-//            is SeriesViewHolder -> onBindSeries(holder,position)
-//        }
-//    }
-//    override fun getItemViewType(position: Int): Int {
-//        return when(itemsList[position].searchItemType){
-//            SearchItemType.TYPE_SERIES -> VIEW_SERIES_HEADER
-//        }
-//    }
-//   private companion object{
-//       const val VIEW_SERIES_HEADER=1
-//   }
-//
 //}
-//
-//
+
+
+import androidx.databinding.DataBindingUtil
+import com.chocolatecake.marvel.databinding.ItemCharacterBinding
+import com.chocolatecake.marvel.databinding.ItemComicBinding
+
+
+import com.chocolatecake.marvel.databinding.ItemSeriesBinding
+
+
+class SearchAdapter(
+   var itemsList:List<SearchItems>,
+    listener:SearchInteractionListener
+    ):BaseAdapter<SearchItems>(itemsList,listener) {
+    override val layoutId: Int
+        get() = R.layout.item_series
+    fun updateList(newItems: List<SearchItems>) {
+        val oldItems = itemsList
+        itemsList = newItems
+        val diffResult = DiffUtil.calculateDiff(DiffUtils(oldItems, newItems))
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class SeriesViewHolder(var itemSeriesBinding:ItemSeriesBinding ):BaseViewHolder(itemSeriesBinding)
+    private fun onBindSeries(searchHeaderViewHolder: SeriesViewHolder, position:Int){
+        val header = itemsList[position] as SearchItems.SeriesItem
+        searchHeaderViewHolder.itemSeriesBinding.item=header.series
+    }
+    class ComicsViewHolder(var ItemComicBinding:ItemComicBinding ):BaseViewHolder(com.chocolatecake.marvel.databinding.ItemComicBinding)
+    private fun onBindComics(searchHeaderViewHolder: ComicsViewHolder, position:Int){
+        val header = itemsList[position] as SearchItems.ComicsItem
+        searchHeaderViewHolder.ItemComicBinding.item=header.comics
+    }
+    class CharactersViewHolder(var itemSeriesBinding:ItemSeriesBinding ):BaseViewHolder(itemSeriesBinding)
+    private fun onBindCharacters(searchHeaderViewHolder: SeriesViewHolder, position:Int){
+        val header = itemsList[position] as SearchItems.SeriesItem
+        searchHeaderViewHolder.itemSeriesBinding.item=header.series
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return when(viewType){
+             VIEW_SERIES->{
+                 val binding=DataBindingUtil.inflate<ItemSeriesBinding>(
+                     LayoutInflater.from(parent.context),
+                     R.layout.item_series,
+                     parent,
+                     false)
+                 SeriesViewHolder(binding)
+             }
+            VIEW_CHARACTERS ->
+                DataBindingUtil.inflate<ItemCharacterBinding>(
+                LayoutInflater.from(parent.context),
+                R.layout.item_character,
+                parent,
+                false)
+            SeriesViewHolder(binding)
+            else -> throw Exception("Invalid ViewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        when(holder){
+            is SeriesViewHolder -> onBindSeries(holder,position)
+        }
+    }
+    override fun getItemViewType(position: Int): Int {
+        return when(itemsList[position].searchItemType){
+            SearchItemType.TYPE_SERIES -> VIEW_SERIES
+            SearchItemType.TYPE_Comics -> VIEW_COMICS
+            SearchItemType.TYPE_Character -> VIEW_CHARACTERS
+        }
+    }
+   private companion object{
+       const val VIEW_SERIES=0
+       const val VIEW_COMICS=1
+       const val VIEW_CHARACTERS=2
+
+   }
+
+}
+
+
--- app/src/main/java/com/chocolatecake/marvel/ui/search/SearchItems.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/search/SearchItems.kt
@@ -6,8 +6,12 @@ import com.chocolatecake.marvel.data.model.SeriesResult
 
 sealed class  SearchItems(val searchItemType:SearchItemType ){
  data class SeriesItem(val series: SeriesResult) : SearchItems(SearchItemType.TYPE_SERIES)
+    data class ComicsItem(val comics: ComicsResult) : SearchItems(SearchItemType.TYPE_Comics)
+
+    data class CharacterItem(val character: ProfileResult) : SearchItems(SearchItemType.TYPE_Character)
+
 }
 enum class SearchItemType {
-  TYPE_SERIES;
+  TYPE_SERIES,TYPE_Comics,TYPE_Character;
 }
 
