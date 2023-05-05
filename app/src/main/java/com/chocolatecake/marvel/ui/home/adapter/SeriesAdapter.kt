c8f02f097d736d8efb76b5fbe39a9f344707b3ed
Menna Akram <mennaakram976@gmail.com>
2023-05-05 17:55:33 +0300

    add series adapter

---
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/adapter/SeriesAdapter.kt
@@ -0,0 +1,12 @@
+package com.chocolatecake.marvel.ui.home.adapter
+
+import com.chocolatecake.marvel.R
+import com.chocolatecake.marvel.data.model.SeriesResult
+import com.chocolatecake.marvel.ui.base.BaseAdapter
+import com.chocolatecake.marvel.ui.home.view.HomeListener
+
+class SeriesAdapter(itemsSeries: List<SeriesResult?>, listener: HomeListener) :
+    BaseAdapter<SeriesResult?>(itemsSeries, listener) {
+    override val layoutId: Int
+        get() = R.layout.item_series
+}
