2b98ffe36eaf3bfec0a9f4c61f50401590379b26
Menna Akram <mennaakram976@gmail.com>
2023-05-05 17:59:26 +0300

    add home series to home fragment

--- app/src/main/java/com/chocolatecake/marvel/ui/home/view_model/HomeViewModel.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/view_model/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.chocolatecake.marvel.ui.home.view_model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.chocolatecake.marvel.data.model.EventResult
+import com.chocolatecake.marvel.data.model.SeriesResult
 import com.chocolatecake.marvel.data.model.base.BaseResponse
 import com.chocolatecake.marvel.data.repository.MarvelRepository
 import com.chocolatecake.marvel.data.repository.MarvelRepositoryImpl
@@ -16,8 +17,17 @@ class HomeViewModel : BaseViewModel(), HomeListener {
     private val _events = MutableLiveData<Status<List<EventResult>>>()
     val events: LiveData<Status<List<EventResult>>> get() = _events
 
+    private val _series = MutableLiveData<Status<List<SeriesResult>>>()
+    val series: LiveData<Status<List<SeriesResult>>> get() = _series
+
     private val _eventId = MutableLiveData<Int?>()
     val eventId: LiveData<Int?> get() = _eventId
+
+    private val _seriesId = MutableLiveData<Int?>()
+    val seriesId: LiveData<Int?> get() = _seriesId
+
+    private val _navigateToSeries = MutableLiveData(false)
+    val navigateToSeries: LiveData<Boolean> get() = _navigateToSeries
 
     init {
         loadData()
@@ -25,6 +35,7 @@ class HomeViewModel : BaseViewModel(), HomeListener {
 
     fun loadData() {
         getCurrentEvent()
+        getCurrentSeries()
     }
 
     private fun getCurrentEvent() {
@@ -39,13 +50,32 @@ class HomeViewModel : BaseViewModel(), HomeListener {
         }
     }
 
+    private fun getCurrentSeries() {
+        _series.postValue(Status.Loading)
+        marvelRepository.getSeries(limit = 8).subscribe(::onSeriesSuccess, ::onFailure).add()
+    }
+
+    private fun onSeriesSuccess(status: Status<BaseResponse<SeriesResult>?>) {
+        status.toData()?.data?.results?.let {
+            _series.postValue(Status.Success(it.filterNotNull()))
+        }
+    }
 
     private fun onFailure(throwable: Throwable) {
         _events.postValue(Status.Failure(throwable.message.toString()))
+        _series.postValue(Status.Failure(throwable.message.toString()))
     }
 
     override fun onClickBanner(eventId: Int?) {
         _eventId.postValue(eventId)
+    }
+
+    override fun onClickSeries(seriesId: Int?) {
+        _seriesId.postValue(seriesId)
+    }
+
+    override fun onClickMoreSeries() {
+        _navigateToSeries.postValue(true)
     }
 
 }
