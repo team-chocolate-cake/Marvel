92fdefb2d487da72c0ca71af713c614cc995590e
Saif Alhaider <saifharith90@gmail.com>
2023-05-05 23:34:28 +0300

    remove empty space

--- app/src/main/java/com/chocolatecake/marvel/ui/character_details/CharacterDetailsAdapter.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/character_details/CharacterDetailsAdapter.kt
@@ -28,7 +28,6 @@ class CharacterDetailsAdapter(
                     )
                 )
             }
-
             R.layout.character_comics -> {
                 CharacterComicsViewHolder(
                     DataBindingUtil.inflate(
