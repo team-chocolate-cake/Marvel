99a2ee4987adae0800f35616dd66e7c8c5adff80
mohammed-akkad <momoo362514@gmail.com>
2023-05-05 14:50:02 +0300

    update binding adapters

--- app/src/main/java/com/chocolatecake/marvel/ui/util/BindingAdapters.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/util/BindingAdapters.kt
@@ -4,25 +4,53 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.chocolatecake.marvel.ui.base.BaseAdapter
 import android.view.View
-import androidx.core.view.isVisible
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.chocolatecake.marvel.R
+import com.chocolatecake.marvel.data.model.ImageResponse
 import com.chocolatecake.marvel.data.util.Status
 
-@BindingAdapter(value =["app:items"])
-fun <T> RecyclerView.setRecyclerItems(items:List<T>?) {
+@BindingAdapter(value = ["app:items"])
+fun <T> RecyclerView.setRecyclerItems(items: List<T>?) {
     (adapter as BaseAdapter<T>).setItems(items ?: emptyList())
 }
 
 @BindingAdapter(value = ["app:showWhenSuccess"])
 fun <T> View.showWhenSuccess(status: Status<T>?) {
-    this.isVisible = (status is Status.Success)
+    visibility = if ((status is Status.Success)) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
 }
 
 @BindingAdapter(value = ["app:showWhenFailure"])
 fun <T> View.showWhenFailure(status: Status<T>?) {
-    this.isVisible = (status is Status.Failure)
+    visibility = if ((status is Status.Failure)) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
 }
 
 @BindingAdapter(value = ["app:showWhenLoading"])
 fun <T> View.showWhenLoading(status: Status<T>?) {
-    this.isVisible = (status is Status.Loading)
+    visibility = if ((status is Status.Loading)) {
+        View.VISIBLE
+    } else {
+        View.GONE
+    }
+}
+
+@BindingAdapter(value = ["app:imageUrl"])
+fun ImageView.loadImage(imageResponse: ImageResponse?) {
+    val url = if (imageResponse?.path?.split("/")?.last() == "image_not_available") {
+        "https://fandomwire.com/wp-content/uploads/2018/07/Marvel-Logo-14.jpg"
+    } else {
+        imageResponse?.toUrl()
+    }
+    Glide.with(context)
+        .load(url)
+        .thumbnail(Glide.with(context).load(R.raw.loading))
+        .into(this)
 }
