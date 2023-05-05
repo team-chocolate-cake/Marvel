83c052a0d1e97f06adde85d5fe8ce794578e8b61
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-05-05 21:38:22 +0300

    we finished of home item

--- app/src/main/java/com/chocolatecake/marvel/ui/home/model/HomeItem.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/model/HomeItem.kt
@@ -1,9 +1,11 @@
 package com.chocolatecake.marvel.ui.home.model
 
+import com.chocolatecake.marvel.data.model.ComicsResult
 import com.chocolatecake.marvel.data.model.EventResult
 import com.chocolatecake.marvel.data.model.SeriesResult
 
 sealed class HomeItem(val priority: Int){
     data class EventsItem(val eventResult: List<EventResult?>): HomeItem(0)
     data class SeriesItem(val seriesResult: List<SeriesResult?>): HomeItem(1)
+    data class ComicItem(val comicResult : ComicsResult) : HomeItem(2)
 }
