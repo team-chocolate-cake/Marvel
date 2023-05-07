80cc84b03309e2bc93691e5e911c6bed632a1e8b
mohammed-akkad <momoo362514@gmail.com>
2023-05-07 20:06:18 +0300

    make companion object instead of static number

--- app/src/main/java/com/chocolatecake/marvel/ui/home/model/HomeItem.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/home/model/HomeItem.kt
@@ -5,7 +5,13 @@ import com.chocolatecake.marvel.data.model.EventResult
 import com.chocolatecake.marvel.data.model.SeriesResult
 
 sealed class HomeItem(val priority: Int){
-    data class EventsItem(val eventResult: List<EventResult?>): HomeItem(0)
-    data class SeriesItem(val seriesResult: List<SeriesResult?>): HomeItem(1)
-    data class ComicItem(val comicResult : ComicsResult) : HomeItem(2)
+    data class EventsItem(val eventResult: List<EventResult?>) : HomeItem(VIEW_TYPE_HEADER)
+    data class SeriesItem(val seriesResult: List<SeriesResult?>) : HomeItem(VIEW_TYPE_SERIES)
+    data class ComicItem(val comicResult: ComicsResult) : HomeItem(VIEW_TYPE_COMIC)
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 0
+        private const val VIEW_TYPE_SERIES = 1
+        private const val VIEW_TYPE_COMIC = 2
+    }
 }
