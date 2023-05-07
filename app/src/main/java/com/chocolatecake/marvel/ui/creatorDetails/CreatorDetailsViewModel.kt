895e816264dfb8d7ac0226d5cd82944e56390373
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-07 17:44:55 +0300

    enhance

--- app/src/main/java/com/chocolatecake/marvel/ui/creatorDetails/CreatorDetailsViewModel.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/creatorDetails/CreatorDetailsViewModel.kt
@@ -1,6 +1,5 @@
 package com.chocolatecake.marvel.ui.creatorDetails
 
-import android.annotation.SuppressLint
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.chocolatecake.marvel.data.model.ComicsResult
@@ -11,35 +10,39 @@ import com.chocolatecake.marvel.data.repository.MarvelRepositoryImpl
 import com.chocolatecake.marvel.data.util.Status
 import com.chocolatecake.marvel.ui.base.BaseViewModel
 
-class CreatorDetailsViewModel : BaseViewModel(),CreatorDetailsListener {
-   val comicsList = MutableLiveData<Status<List<ComicsResult>?>>()
+class CreatorDetailsViewModel : BaseViewModel(), CreatorDetailsListener {
+    val comicsList = MutableLiveData<Status<List<ComicsResult>?>>()
     val seriesList = MutableLiveData<Status<List<SeriesResult>?>>()
-    val  creator=MutableLiveData<Status<List<ProfileResult>?>>()
+    val creator = MutableLiveData<Status<List<ProfileResult>?>>()
 
 
     private val repository: MarvelRepository by lazy {
         MarvelRepositoryImpl()
     }
+    private val creatorId: Int? = null
+
     init {
-        getComics()
-        getSeries()
-        getCreator()
+        loadData(creatorId ?: 1)
     }
 
-    private fun getComics() {
-        repository.getComicsForCreator(13970).subscribe({
-            it.toData()?.data?.results.let {
-                comicsList.postValue(Status.Success(it?.filterNotNull()))
-                Log.d("nahed", it.toString())
+    fun loadData(id: Int) {
+        getCreator(id)
+        getSeries(id)
+        getComics(id)
+    }
 
+    private fun getCreator(id: Int) {
+        repository.getCreatorById(id).subscribe({
+            it.toData()?.data?.results.let {
+                creator.postValue(Status.Success(it?.filterNotNull()))
             }
         }, {
             Log.d("nahed", it.toString())
         }).add()
     }
 
-    private fun getSeries() {
-        repository.getSeriesForCreator(13970).subscribe({
+    private fun getSeries(id: Int) {
+        repository.getSeriesForCreator(id).subscribe({
             it.toData()?.data?.results.let {
                 seriesList.postValue(Status.Success(it?.filterNotNull()))
                 Log.d("nahed", it.toString())
@@ -49,12 +52,15 @@ class CreatorDetailsViewModel : BaseViewModel(),CreatorDetailsListener {
             Log.d("nahed", it.toString())
         }).add()
     }
-    private fun getCreator(){
-        repository.getCreatorById(13970).subscribe({
+
+    private fun getComics(id: Int) {
+        repository.getComicsForCreator(id).subscribe({
             it.toData()?.data?.results.let {
-                creator.postValue(Status.Success(it?.filterNotNull()))
+                comicsList.postValue(Status.Success(it?.filterNotNull()))
+                Log.d("nahed", it.toString())
+
             }
-        },{
+        }, {
             Log.d("nahed", it.toString())
         }).add()
     }
