86438f22294a9dd52aff115ac579535fa485f74b
Najeia Tammous <ntammous11@gmail.com>
2023-05-11 15:52:50 +0300

    Implemented Stories List Fragment

--- app/src/main/java/com/chocolatecake/marvel/ui/stories/StoriesViewModel.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/stories/StoriesViewModel.kt
@@ -1,6 +1,47 @@
 package com.chocolatecake.marvel.ui.stories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.chocolatecake.marvel.data.model.StoryResult
+import com.chocolatecake.marvel.data.repository.MarvelRepository
+import com.chocolatecake.marvel.data.repository.MarvelRepositoryImpl
+import com.chocolatecake.marvel.data.util.Status
 import com.chocolatecake.marvel.ui.base.BaseViewModel
+import com.chocolatecake.marvel.ui.core.listener.StoryListener
 
-class StoriesViewModel :BaseViewModel(){
+class StoriesViewModel : BaseViewModel(), StoryListener {
+
+    val repository: MarvelRepository by lazy { MarvelRepositoryImpl() }
+    private val _stories = MutableLiveData<Status<List<StoryResult>>>()
+    val stories: LiveData<Status<List<StoryResult>>>
+        get() = _stories
+
+    private val _storiesId = MutableLiveData<Int?>()
+    val seriesId: LiveData<Int?> get() = _storiesId
+    init {
+        loadData()
+    }
+
+    fun loadData() {
+        getStories()
+    }
+
+    private fun getStories() {
+        repository.getStories(limit = 30)
+            .subscribe(::onStoriesSuccess, ::onFailure).add()
+    }
+
+    private fun onStoriesSuccess(status: Status<List<StoryResult>>) {
+        status.toData()?.let {
+            _stories.postValue(Status.Success(it))
+        }
+    }
+
+    private fun onFailure(throwable: Throwable) {
+        _stories.postValue(Status.Failure(throwable.message.toString()))
+    }
+
+    override fun onClickStory(id: Int) {
+        _storiesId.postValue(id)
+    }
 }
