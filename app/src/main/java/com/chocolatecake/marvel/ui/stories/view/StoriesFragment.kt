f943349b8a6d724f66d820b7d3cee3ce5c54f417
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-11 18:28:31 +0300

    refactor names

--- app/src/main/java/com/chocolatecake/marvel/ui/stories/view/StoriesFragment.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/stories/view/StoriesFragment.kt
@@ -4,15 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.chocolatecake.marvel.R
-import com.chocolatecake.marvel.databinding.StoriesFragmentBinding
+import com.chocolatecake.marvel.databinding.FragmentStoriesBinding
 import com.chocolatecake.marvel.ui.base.BaseFragment
 import com.chocolatecake.marvel.ui.stories.viewModel.StoriesViewModel
 import com.chocolatecake.marvel.ui.stories.adapter.StoriesAdapter
 
-class StoriesFragment : BaseFragment<StoriesFragmentBinding, StoriesViewModel>() {
+class StoriesFragment : BaseFragment<FragmentStoriesBinding, StoriesViewModel>() {
 
     override val viewModel: StoriesViewModel by viewModels()
-    override val layoutIdFragment: Int = R.layout.stories_fragment
+    override val layoutIdFragment: Int = R.layout.fragment_stories
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/chocolatecake/marvel/ui/storiesDetails/view/StoriesDetailsFragment.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/storiesDetails/view/StoriesDetailsFragment.kt
@@ -4,17 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.chocolatecake.marvel.R
-import com.chocolatecake.marvel.databinding.StoriesDetailsFragmentBinding
+import com.chocolatecake.marvel.databinding.FragmentStoriesDetailsBinding
 import com.chocolatecake.marvel.ui.base.BaseFragment
 import com.chocolatecake.marvel.ui.storiesDetails.adapter.ComicsAdapter
 import com.chocolatecake.marvel.ui.storiesDetails.adapter.CreatorsAdapter
 import com.chocolatecake.marvel.ui.storiesDetails.adapter.SeriesAdapter
 import com.chocolatecake.marvel.ui.storiesDetails.view_model.StoriesDetailsViewModel
 
-class StoriesDetailsFragment  :BaseFragment<StoriesDetailsFragmentBinding, StoriesDetailsViewModel>(){
+class StoriesDetailsFragment  :BaseFragment<FragmentStoriesDetailsBinding, StoriesDetailsViewModel>(){
     override val viewModel: StoriesDetailsViewModel by viewModels()
     override val layoutIdFragment: Int
-        get() = R.layout.stories_details_fragment
+        get() = R.layout.fragment_stories_details
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
