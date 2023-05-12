e654760de6afbdfe1d8b3ae37688b3eee8e744e2
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-12 23:25:37 +0300

    cleanup

--- app/src/main/java/com/chocolatecake/marvel/ui/search/view/SearchFragment.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/search/view/SearchFragment.kt
@@ -2,15 +2,17 @@ package com.chocolatecake.marvel.ui.search.view
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.chocolatecake.marvel.R
 import com.chocolatecake.marvel.data.util.Status
 import com.chocolatecake.marvel.databinding.FragmentSeacrhBinding
 import com.chocolatecake.marvel.ui.base.BaseFragment
-import com.chocolatecake.marvel.ui.search.model.SearchItemType.*
-import com.chocolatecake.marvel.ui.search.model.SearchItems
 import com.chocolatecake.marvel.ui.search.SearchListAdapter
+import com.chocolatecake.marvel.ui.search.model.SearchItemType.TYPE_CHARACTER
+import com.chocolatecake.marvel.ui.search.model.SearchItemType.TYPE_COMICS
+import com.chocolatecake.marvel.ui.search.model.SearchItemType.TYPE_SERIES
+import com.chocolatecake.marvel.ui.search.model.SearchItems
 import com.chocolatecake.marvel.ui.search.view_model.SearchViewModel
 import com.chocolatecake.marvel.util.observeNonNull
 
@@ -31,26 +33,36 @@ class SearchFragment : BaseFragment<FragmentSeacrhBinding, SearchViewModel>() {
 
     private fun observeListItems() {
         viewModel.state.observeNonNull(viewLifecycleOwner) {
+            var count = 2
             if (it is Status.Success) {
+
                 when (viewModel.searchType) {
                     TYPE_SERIES -> adapter.submitList(it.data.series.map { series ->
+                        count = 2
                         SearchItems.SeriesItem(
                             series
                         )
+
                     })
 
                     TYPE_COMICS -> adapter.submitList(it.data.comics.map { comic ->
+                        count = 2
                         SearchItems.ComicsItem(
                             comic
                         )
                     })
 
                     TYPE_CHARACTER -> adapter.submitList(it.data.characters.map { character ->
+                        count = 3
                         SearchItems.CharacterItem(
                             character
                         )
                     })
                 }
+                binding.recyclerView.layoutManager = GridLayoutManager(
+                    requireContext(),
+                    count
+                )
             }
         }
     }
