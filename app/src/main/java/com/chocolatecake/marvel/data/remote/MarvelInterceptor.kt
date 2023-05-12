79867d7436f8ec8818850eed228f3dfe25e499b3
Najeia Tammous <ntammous11@gmail.com>
2023-05-12 17:18:30 +0300

    Refactor Hashing and Interceptor

--- app/src/main/java/com/chocolatecake/marvel/data/remote/MarvelInterceptor.kt
+++ app/src/main/java/com/chocolatecake/marvel/data/remote/MarvelInterceptor.kt
@@ -3,32 +3,20 @@ package com.chocolatecake.marvel.data.remote
 import com.chocolatecake.marvel.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
-import java.security.MessageDigest
-
-
 class MarvelInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val timestamp = System.currentTimeMillis().toString()
-        val hash = generateHash(timestamp)
+
+        val hashed = HashGenerator.generateHash(BuildConfig.PRIVATE_KEY, BuildConfig.PUBLIC_KEY)
 
         val originalRequest = chain.request()
         val url = originalRequest.url.newBuilder()
-            .addQueryParameter("ts", timestamp)
+            .addQueryParameter("ts", HashGenerator.timestamp)
             .addQueryParameter("apikey", BuildConfig.PUBLIC_KEY)
-            .addQueryParameter("hash", hash)
+            .addQueryParameter("hash", hashed)
             .build()
 
-        val requestBuilder = originalRequest.newBuilder()
-            .url(url)
+        val requestBuilder = originalRequest.newBuilder().url(url)
 
         return chain.proceed(requestBuilder.build())
-    }
-
-    private fun generateHash(timestamp: String): String {
-        val md5 = MessageDigest.getInstance("MD5")
-        val input = "$timestamp${BuildConfig.PRIVATE_KEY}${BuildConfig.PUBLIC_KEY}"
-        val digest = md5.digest(input.toByteArray())
-
-        return digest.joinToString("") { "%02x".format(it) }
     }
 }
