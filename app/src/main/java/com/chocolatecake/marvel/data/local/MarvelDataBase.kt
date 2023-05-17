d5a4bbf5fdf520b7e06779502df1d4004eedceb8
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-05-17 19:41:50 +0300

    add abstract seriesDao

--- app/src/main/java/com/chocolatecake/marvel/data/local/MarvelDataBase.kt
+++ app/src/main/java/com/chocolatecake/marvel/data/local/MarvelDataBase.kt
@@ -3,11 +3,13 @@ package com.chocolatecake.marvel.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.chocolatecake.marvel.data.local.dao.CharacterDao
+import com.chocolatecake.marvel.data.local.dao.SeriesDao
 import com.chocolatecake.marvel.data.local.entities.CharacterEntity
 import com.chocolatecake.marvel.data.local.entities.ComicsEntity
 import com.chocolatecake.marvel.data.local.entities.EventEntity
 import com.chocolatecake.marvel.data.local.entities.SeriesEntity
 import com.chocolatecake.marvel.data.local.entities.StoryEntity
+import com.chocolatecake.marvel.data.remote.model.dto.SeriesDto
 
 @Database(
     entities = [
@@ -22,4 +24,5 @@ import com.chocolatecake.marvel.data.local.entities.StoryEntity
 )
 abstract class MarvelDataBase : RoomDatabase() {
     abstract val characterDao: CharacterDao
+    abstract val seriesDao : SeriesDao
 }
