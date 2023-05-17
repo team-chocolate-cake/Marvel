d078a68dff476174fd64fb6b7f33d7d24de7662d
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-05-17 19:40:52 +0300

    init of SeriesDao

---
+++ app/src/main/java/com/chocolatecake/marvel/data/local/dao/SeriesDao.kt
@@ -0,0 +1,12 @@
+package com.chocolatecake.marvel.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.chocolatecake.marvel.data.local.entities.SeriesEntity
+import io.reactivex.rxjava3.core.Single
+
+@Dao
+interface SeriesDao{
+
+}
