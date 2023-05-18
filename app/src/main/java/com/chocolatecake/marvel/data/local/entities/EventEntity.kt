f1a8d513a17d411bd577e5b96faacdccc97d7d5b
mohammed-akkad <momoo362514@gmail.com>
2023-05-18 17:00:54 +0300

    add comic and event entity

--- app/src/main/java/com/chocolatecake/marvel/data/local/entities/EventEntity.kt
+++ app/src/main/java/com/chocolatecake/marvel/data/local/entities/EventEntity.kt
@@ -3,7 +3,7 @@ package com.chocolatecake.marvel.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "TABLE_EVENT")
 data class EventEntity(
     @PrimaryKey(autoGenerate = false)
     val id: Int,
