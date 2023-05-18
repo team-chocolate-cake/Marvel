2f2ea6a9c82d7ff8ff2c4f119897a95bd266a94a
Hasa9nAli <hasan.20cs75@student.uomosul.edu.iq>
2023-05-18 15:42:51 +0300

    complete series

--- app/src/main/java/com/chocolatecake/marvel/data/local/dao/SeriesDao.kt
+++ app/src/main/java/com/chocolatecake/marvel/data/local/dao/SeriesDao.kt
@@ -2,12 +2,22 @@ package com.chocolatecake.marvel.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.chocolatecake.marvel.data.local.entities.SeriesEntity
+import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 
 @Dao
 interface SeriesDao{
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSeries(series : List<SeriesEntity>) : Completable
+
     @Query("SELECT * FROM SeriesEntity")
     fun getAllSeries(): Single<List<SeriesEntity>>
+
+    @Query("DELETE FROM SeriesEntity")
+    fun deleteAllSeries() : Completable
+
 }
