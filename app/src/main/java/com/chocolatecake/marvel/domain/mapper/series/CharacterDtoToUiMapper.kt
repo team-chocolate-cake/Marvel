88f532fb35a43792e93b481025e2070b3f160e15
Mohamed Elgohary <bondokelgohary@gmail.com>
2023-05-19 20:26:35 +0300

    complete mapping series

---
+++ app/src/main/java/com/chocolatecake/marvel/domain/mapper/series/CharacterDtoToUiMapper.kt
@@ -0,0 +1,18 @@
+package com.chocolatecake.marvel.domain.mapper.series
+
+import com.chocolatecake.marvel.data.remote.model.dto.ProfileDto
+import com.chocolatecake.marvel.domain.mapper.Mapper
+import com.chocolatecake.marvel.domain.model.Character
+import dagger.hilt.android.scopes.ViewModelScoped
+import javax.inject.Inject
+
+@ViewModelScoped
+class CharacterDtoToUiMapper @Inject constructor() : Mapper<ProfileDto, Character> {
+    override fun map(input: ProfileDto): Character {
+        return Character(
+            id = input.id ?: 0,
+            name = input.name ?: "",
+            imageURL = input.thumbnail?.toUrl() ?: ""
+        )
+    }
+}
