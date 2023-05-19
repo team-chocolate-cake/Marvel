9e6dc2544f46bed5d736fc8380b102ef0acc8915
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-19 23:57:19 +0300

    solve issue

--- app/src/main/java/com/chocolatecake/marvel/ui/creator_details/adapter/SeriesAdapter.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/creator_details/adapter/SeriesAdapter.kt
@@ -1,11 +1,11 @@
 package com.chocolatecake.marvel.ui.creator_details.adapter
 
 import com.chocolatecake.marvel.R
-import com.chocolatecake.marvel.data.remote.model.dto.SeriesDto
+import com.chocolatecake.marvel.domain.model.Series
 import com.chocolatecake.marvel.ui.base.BaseAdapter
 
-class SeriesAdapter(seriesList: List<SeriesDto>, listener: CreatorDetailsListener,):
-    BaseAdapter<SeriesDto?>(seriesList,listener) {
+class SeriesAdapter(seriesList: List<Series>, listener: CreatorDetailsListener,):
+    BaseAdapter<Series>(seriesList,listener) {
 
     override val layoutId = R.layout.series_item
 }
