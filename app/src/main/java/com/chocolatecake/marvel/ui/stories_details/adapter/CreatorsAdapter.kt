21d641abaa48c08fcba912ea71e39a9c4d6c56ad
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-05-20 01:28:50 +0300

    solve merge conflict

--- app/src/main/java/com/chocolatecake/marvel/ui/stories_details/adapter/CreatorsAdapter.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/stories_details/adapter/CreatorsAdapter.kt
@@ -1,12 +1,12 @@
 package com.chocolatecake.marvel.ui.stories_details.adapter
 
 import com.chocolatecake.marvel.R
-import com.chocolatecake.marvel.domain.model.CreatorDetails
+import com.chocolatecake.marvel.domain.model.Creator
 import com.chocolatecake.marvel.ui.base.BaseAdapter
 import com.chocolatecake.marvel.ui.core.listener.CreatorsListener
 
-class CreatorsAdapter(creatorList: List<CreatorDetails>, listener: CreatorsListener) :
-    BaseAdapter<CreatorDetails>(creatorList, listener) {
+class CreatorsAdapter(creatorList: List<Creator>, listener: CreatorsListener) :
+    BaseAdapter<Creator>(creatorList, listener) {
 
     override val layoutId = R.layout.item_creator
 }
--- app/src/main/java/com/chocolatecake/marvel/ui/stories_details/adapter/SeriesAdapter.kt
+++ app/src/main/java/com/chocolatecake/marvel/ui/stories_details/adapter/SeriesAdapter.kt
@@ -1,12 +1,12 @@
 package com.chocolatecake.marvel.ui.stories_details.adapter
 
 import com.chocolatecake.marvel.R
-import com.chocolatecake.marvel.data.remote.model.dto.SeriesDto
+import com.chocolatecake.marvel.domain.model.Series
 import com.chocolatecake.marvel.ui.base.BaseAdapter
 import com.chocolatecake.marvel.ui.core.listener.SeriesListener
 
-class SeriesAdapter (seriesList: List<SeriesDto>, listener: SeriesListener) :
-    BaseAdapter<SeriesDto>(seriesList, listener) {
+class SeriesAdapter (seriesList: List<Series>, listener: SeriesListener) :
+    BaseAdapter<Series>(seriesList, listener) {
 
     override val layoutId = R.layout.series_item
 }
